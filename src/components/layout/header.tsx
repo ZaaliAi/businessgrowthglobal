92e9efc0aca932fd082fdcd7741fb9330860cb40
Darrel Alison <darrelalison49@gmail.com>
2025-08-28 19:11:49 +0000

    thats better also needs to move right slightly so the left side isnt tou

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -17,7 +17,7 @@ export default async function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center justify-between">
-        <Link href="/" className="mr-6 flex items-center">
+        <Link href="/" className="flex items-center space-x-2 mr-6 ml-2">
           <Image 
             src="https://loirhexauyamqihgphsu.supabase.co/storage/v1/object/public/site-images/Business-Global-Growth-logo.png" 
             alt="Business Growth Global Logo"
